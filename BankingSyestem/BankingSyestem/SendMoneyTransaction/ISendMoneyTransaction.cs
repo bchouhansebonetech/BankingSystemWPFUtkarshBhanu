0264cd551a8adb0b862310c2f23b84aa576726a6
bchouhansebonetech <bchouhan@sebonetechnologies.com>
2022-04-19 11:55:28 +0530

    second commit of Banking System

---
+++ BankingSyestem/BankingSyestem/SendMoneyTransaction/ISendMoneyTransaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSyestem.SendMoneyTransaction
+{
+    internal interface ISendMoneyTransaction
+    {
+    }
+}
