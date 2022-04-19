9d3e37ddd635908d07041ea147c5e64ab557f25d
bchouhansebonetech <bchouhan@sebonetechnologies.com>
2022-04-19 18:34:50 +0530

    Bhanu Commit after revert

---
+++ BankingSyestem/BankingSyestem/LogIn/LogInImpl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSyestem.LogIn
+{
+    public class LogInImpl : ILogIn
+    {
+
+        public UserLoginDataDO loginAccess(UserLoginDataDO userLoginDataDO, List<UserLoginDataDO> userLoginDataDOList)
+        {
+            if (userLoginDataDO.Status == true)
+                return userLoginDataDO;
+            else { 
+            if (userLoginDataDO.UserName == null || userLoginDataDO.Password == null)
+                userLoginDataDO.Status = false;
+            else
+            {
+                foreach (UserLoginDataDO userLoginDatado in userLoginDataDOList)
+                {
+                    if (userLoginDatado.UserName == userLoginDataDO.UserName && userLoginDataDO.Password == userLoginDatado.Password)
+                        userLoginDataDO.Status = true;
+                    break;
+                }
+            }
+            return userLoginDataDO;
+        }
+    } }
+}
--- BankingSyestem/BankingSyestem/SignUp/SignUpImpl.cs
+++ BankingSyestem/BankingSyestem/SignUp/SignUpImpl.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankingSyestem.LogIn
+namespace BankingSyestem.SignUp
 {
-    public interface ILogIn
+    public class SignUpImpl
     {
+
     }
 }
